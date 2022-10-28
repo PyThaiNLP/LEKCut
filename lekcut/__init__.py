6e1aab7d7323a2c854fe4ea7fa48a15373721f4c
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-10-28 20:44:11 +0700

    Add typing

--- lekcut/__init__.py
+++ lekcut/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 __version__ = "0.1"
+from typing import List
 from lekcut.deepcut import tokenize
 
 
-def word_tokenize(text: str, model: str="deepcut", path: str="default"):
+def word_tokenize(text: str, model: str="deepcut", path: str="default") -> List[str]:
     if model != "deepcut":
         raise NotImplementedError("Not support {} model.".format(model))
     return tokenize(text, path=path)
